3abdc75d82e689ce2a4a00e7e123befbd5b95ea4
sula234 <sult.ka@mail.ru>
2023-12-02 00:32:30 +0600

    Wrote a test and training functions. First, you need to test the accuracy of the neural networks on dataset tests; the second method starts training cycles for the neural networks.

---
+++ utils/operations.py
@@ -0,0 +1,36 @@
+import torch
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+print(f'Using device={device}')
+
+def validate(net,test_loader):
+  count=acc=0
+  for xo,yo in test_loader:
+    x,y = xo.to(device), yo.to(device)
+    with torch.no_grad():
+      p = net.forward(x)
+      _,predicted = torch.max(p,1)
+      acc+=(predicted==y).sum()
+      count+=len(x)
+  return acc/count
+
+def train(net,train_loader,test_loader,epochs=3,loss_fn=torch.nn.CrossEntropyLoss()):
+  net = net.to(device)
+  optimizer = torch.optim.Adam(net.parameters())
+  for ep in range(epochs):
+    count=acc=0
+    for xo,yo in train_loader:
+      x = xo.to(device)
+      y = yo.to(device)
+      optimizer.zero_grad()
+      p = net.forward(x)
+      loss = loss_fn(p,y)
+      loss.backward()
+      optimizer.step()
+      _,predicted = torch.max(p,1)
+      acc+=(predicted==y).sum()
+      count+=len(x)
+    val_acc = validate(net,test_loader)
+    print(f"Epoch={ep}, train_acc={acc/count}, val_acc={val_acc}")
