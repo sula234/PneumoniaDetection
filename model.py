da33e27d1a790cf0499d76e27844b18807895761
sula234 <sult.ka@mail.ru>
2023-12-01 23:59:05 +0600

    Added model.py file

---
+++ model.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, 3)
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.pool = nn.MaxPool2d(2)
+        self.dropout = nn.Dropout(0.25)
+        self.dropout1 = nn.Dropout(0.3)
+        self.flat = nn.Flatten()
+        self.fc1 = nn.LazyLinear(128)
+        self.fc2 = nn.Linear(128, 35)
+
+    def forward(self, x):
+        x = self.pool(nn.functional.relu(self.conv1(x)))
+        x = self.dropout(x)
+        x = self.pool(nn.functional.relu(self.conv2(x)))
+        x = self.dropout(x)
+        x = self.pool(nn.functional.relu(self.conv3(x)))
+        x = self.dropout1(x)
+        x = self.flat(x)
+        x = nn.functional.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
