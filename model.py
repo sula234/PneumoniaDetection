376d7d509ae83b2db6a6fcea16b29e2c5aac7756
sula234 <sult.ka@mail.ru>
2023-12-03 00:08:34 +0600

    Added VGG16 to model.py

--- model.py
+++ model.py
@@ -1,8 +1,7 @@
 import torch.nn as nn
 
 
-num_classes = 2
-input_size = 49152
+NUM_CLASSES = 2
 
 
 class CNN(nn.Module):
@@ -16,7 +15,7 @@ class CNN(nn.Module):
         self.dropout1 = nn.Dropout(0.3)
         self.flat = nn.Flatten()
         self.fc1 = nn.Linear(25088, 128)
-        self.fc2 = nn.Linear(128, num_classes)
+        self.fc2 = nn.Linear(128, NUM_CLASSES)
 
     def forward(self, x):
         x = self.pool(nn.functional.relu(self.conv1(x)))
@@ -36,7 +35,7 @@ class CNN(nn.Module):
     
 # Define the architecture of the MLP
 class MLP(nn.Module):
-    def __init__(self):
+    def __init__(self, input_size = 49152):
         super(MLP, self).__init__()
         self.flatten = nn.Flatten()
         self.fc1 = nn.Linear(input_size, 128)
@@ -44,7 +43,7 @@ class MLP(nn.Module):
         self.dropout1 = nn.Dropout(0.5)
         self.fc2 = nn.Linear(128, 64)
         self.dropout2 = nn.Dropout(0.5)
-        self.fc3 = nn.Linear(64, num_classes)
+        self.fc3 = nn.Linear(64, NUM_CLASSES)
 
     def forward(self, x):
         x = self.flatten(x)
@@ -60,3 +59,62 @@ class MLP(nn.Module):
     def get_parameters_amount(self) -> int:
         '''Return the number of parameters of the model'''
         return sum(p.numel() for p in self.parameters())
+    
+
+class VGG16(nn.Module):
+    def __init__(self):
+        super(VGG16, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, NUM_CLASSES)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
