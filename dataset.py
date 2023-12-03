1b78c2f97e09e4b9cca79b0204bfc3d44dfa3b8f
Karina Iskakova <96408272+kariri8@users.noreply.github.com>
2023-12-03 21:33:13 +0600

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -3,6 +3,8 @@ from pathlib import Path
 # prepare loader for train data
 data_dir = Path('data')
 
+
+
 from torch.utils.data import Dataset
 import torchvision
 import torchvision.transforms as transforms
@@ -11,59 +13,133 @@ import os
 import random
 import matplotlib.pyplot as plt
 import torchvision.transforms.functional as F
-import random
+
+class DogsVsCatsAugmentor:
+    def __init__(self, dataset, num_transforms=2):
+        self.dataset = dataset
+        self.num_transforms = num_transforms
+        self.transforms = self._get_transforms()
+
+    def _get_transforms(self):
+        return transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(10),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2)
+        ])
+
+    def augment_dataset(self, save_folder=None, augmentation_fraction=0.001):
+        augmented_data = []
+        total_images = len(self.dataset)
+        num_images_to_augment = int(total_images * augmentation_fraction)
+
+        # Randomly select a starting index for the subset
+        start_idx = random.randint(0, total_images - 1)
+
+        # Calculate the end index for the subset
+        end_idx = min(start_idx + num_images_to_augment, total_images)
+
+        for idx in range(start_idx, end_idx):
+            original_image, label = self.dataset[idx]
+            label = int(label)
+
+            for i in range(self.num_transforms):
+                augmented_image = self.transforms(original_image)
+                augmented_data.append((augmented_image, label))
+
+                # Save the augmented image to the specified folder
+                if save_folder:
+                    label_name = 'cat' if label == 0 else 'dog'
+                    augmented_filename = f'augmented_{idx}_{label_name}_{i}.jpg'
+                    save_path = os.path.join(save_folder, augmented_filename)
+                    torchvision.utils.save_image(augmented_image, save_path)
+
+        return augmented_data
 
 class DogsVsCatsDataset(Dataset):
-    def __init__(self, image_folder, augment_prob=0.4):
+    def __init__(self, image_folder):
         self.image_folder = image_folder
-        self.augment_prob = augment_prob
-
-        self.default_transforms = [
+        self.transforms = transforms.Compose([
             transforms.Resize((128, 128)),
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-        ]
-        self.augumentation = [
-                transforms.RandomHorizontalFlip(),
-                transforms.RandomRotation(10),
-                transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2)
-            ]
-        
-        self.file_list = self._build_file_list()
-        self.dataset_size = len(self.file_list)
-
+        ])
+        self.file_list = os.listdir(image_folder)
 
     def __len__(self):
-        return self.dataset_size
-    
-
-    def _build_file_list(self):
-        file_list = []
-        for class_name in os.listdir(self.image_folder):
-            class_path = os.path.join(self.image_folder, class_name)
-            file_list.extend([(class_name, file) for file in os.listdir(class_path)])
-           
-        return file_list
+        return len(self.file_list)
 
     def __getitem__(self, idx):
-        
-        class_name, img_name = self.file_list[idx]
-        img_path = os.path.join(self.image_folder, class_name, img_name)
-        original_image = Image.open(img_path).convert('RGB')  # Adjust the mode based on your images
+        img_name = os.path.join(self.image_folder, self.file_list[idx])
+        image = Image.open(img_name)
+        image = self.transforms(image)
 
+        # Extract label from the image name
+        if 'dog' in self.file_list[idx]:
+            label = 1
+        elif 'cat' in self.file_list[idx]:
+            label = 0
+        return image, label
 
-        #apply augumentation with given probabilty 
-        if random.random() <= self.augment_prob:
-            new_transforms = self.augumentation + self.default_transforms
-            t = transforms.Compose(new_transforms)
+class DogsVsCatsCompositeDataset(Dataset):
+    def __init__(self, original_dataset, augmented_data):
+        self.original_dataset = original_dataset
+        self.augmented_data = augmented_data
+
+    def __len__(self):
+        return len(self.original_dataset) + len(self.augmented_data)
+
+    def __getitem__(self, idx):
+        if idx < len(self.original_dataset):
+            return self.original_dataset[idx]
         else:
-            t = transforms.Compose(self.default_transforms)
-            
-        original_image = t(original_image)
-        class_label = 0 if class_name == 'cats' else 1  # Assuming 'cat' is class 0 and 'dog' is class 1
+            augmented_idx = idx - len(self.original_dataset)
+            return self.augmented_data[augmented_idx]
 
-        
-        return original_image, class_label
+def denormalize(tensor):
+    # Denormalize the image tensor
+    denormalized_tensor = tensor.clone()
+    denormalized_tensor = denormalized_tensor * 0.5 + 0.5
+    denormalized_tensor = denormalized_tensor.clamp(0, 1)
+    return denormalized_tensor
+
+def visualize_original_and_augmented(index, augmented_folder, original_dataset):
+    # Get the original image and label from the dataset using the provided index
+    original_image, original_label = original_dataset[index]
+
+    denormalized_original = denormalize(original_image)
+    # Display original image
+    plt.subplot(1, 2, 1)
+    plt.imshow(F.to_pil_image(denormalized_original))
+    plt.title(f"Original\nLabel: {original_label}")
+    plt.axis('off')
+    plt.show()
+
+    # Find all augmented images in the specified folder with the given index
+    augmented_files = [f for f in os.listdir(augmented_folder) if f.startswith(f'augmented_{index}_')]
+
+    for i, augmented_file in enumerate(augmented_files):
+        # Load and display each augmented image
+        augmented_image = Image.open(os.path.join(augmented_folder, augmented_file))
+        plt.subplot(1, len(augmented_files) + 1, i + 2)
+        plt.imshow(augmented_image)
+        plt.title(f"Aug {i + 1}")
+        plt.axis('off')
+
+    plt.tight_layout()
+    plt.show()
+
+original_dataset = DogsVsCatsDataset(os.path.join(data_dir, 'train'))
+
+# Create an instance of DogsVsCatsAugmentor
+augmentor = DogsVsCatsAugmentor(original_dataset, num_transforms=2)
+
+# Augment the dataset
+augmented_data = augmentor.augment_dataset(save_folder='augmented_images')
+
+# Create a composite dataset with both original and augmented data
+trainset = DogsVsCatsCompositeDataset(original_dataset, augmented_data)
+
+# visualize_original_and_augmented(index = 23596, augmented_folder = 'augmented_images', original_dataset = composite_dataset)
 
 
 trainset = DogsVsCatsDataset(os.path.join(data_dir, 'train'))
