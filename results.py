4257a2d3bbcb7f3f0367f4b03813cd9b1ff557ee
Karina Iskakova <96408272+kariri8@users.noreply.github.com>
2023-12-04 01:32:35 +0600

    Update results.py

--- results.py
+++ results.py
@@ -7,18 +7,20 @@ from omegaconf import DictConfig
 from torch.utils.data import Dataset
 from torchvision import transforms
 from PIL import Image
-from utils.log import formatter
+from utils.log import formatted
+from test.py import TestDataset
+from test.py import test_dataset
 
 # prepare loader for test data
 data_dir = Path('data')
 
 model_name_to_test = "PretrainedVGG16"  
 test_model = get_model_from_cfg(model_name_to_test)
-test_model.load_state_dict(torch.load("pretrainedvgg16_checkpoint.pth"))  
+test_model.load_state_dict(torch.load("/content/ROBT407Final/models/Exp_0/PretrainedVGG16.pth"))  
 test_model.cuda()
 
 # Define dataloader for testing with the test dataset
-test_dataset = TestDataset(folder_path="path/to/your/test_folder", transform=transforms.ToTensor())
+test_dataset = TestDataset(folder_path="data/test1", transform=transforms.ToTensor())
 testloader = torch.utils.data.DataLoader(test_dataset, batch_size=cfg.batch_size, shuffle=False)
 
 # Specify the path to save the new CSV file
